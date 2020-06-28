1d595cd0eecfa018f714613336a311401f60e493
Yoel M Galarza <yoel.meiker@gmail.com>
2020-06-27 23:23:04 -0300

    Update of app.js
    -Just a little correction in passport.deserealizeUser path

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
-passport.serializeUser(User.deserializerUser());
+passport.deserializeUser(User.deserializeUser());
 
 /* Campground.create({
     name: "Granite Hill",

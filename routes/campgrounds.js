fdf357927bde22d9f5a23ae044d9247b8fe0c233
Yoel M Galarza <yoel.meiker@gmail.com>
2020-07-05 21:45:40 -0300

    Edit and delete comments
    -Make the edit routes put and get  for comments using method override put and create the button edit in the comments.
    -Make the delete route with method override in the button and mongoose .findByIdAndRemove method and add the delete button to the comments.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,8 +1,8 @@
-const   express = require("express");
-const   router = express.Router();
-        methodOverride = require("method-override"),
-        Campground = require("../models/campground"),
-        Comment = require("../models/comments");
+const express = require("express");
+const router = express.Router();
+methodOverride = require("method-override"),
+    Campground = require("../models/campground"),
+    Comment = require("../models/comments");
 
 //INDEX - Show all campgrounds
 router.get("/", function (req, res) {
@@ -12,7 +12,7 @@ router.get("/", function (req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render("campgrounds/index", { campgrounds: allCampgrounds})
+            res.render("campgrounds/index", { campgrounds: allCampgrounds })
         }
     });
 });
@@ -55,29 +55,27 @@ router.get("/:id", function (req, res) {
     });
 });
 //EDIT CAMPGROUND ROUTE
-router.get("/:id/edit", isLoggedIn, (req, res)=>{
-    Campground.findById(req.params.id, (err, foundCampground)=>{ 
-        if (err) {
-            res.redirect("/campgrounds");
-        } else {
-            res.render("campgrounds/edit", {campground: foundCampground});
-        }
+router.get("/:id/edit", checkCampgroundOwnership, (req, res) => {
+    Campground.findById(req.params.id, (err, foundCampground) => {
+        res.render("campgrounds/edit", { campground: foundCampground });
     });
 });
 //UPDATE CAMPGROUND ROUTE
-router.put("/:id", (req, res)=>{
+router.put("/:id", checkCampgroundOwnership, (req, res) => {
     //find and update the correct campground
-    Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updatedCampground)=>{
-        if(err){
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updatedCampground) => {
+        if (err) {
             res.redirect("/campgrounds");
-        } else{
+        } else {
             res.redirect("/campgrounds/" + updatedCampground.id);
         }
     });
     //redirect somewhere(showpage)
 });
+
+
 //DELETE CAMPGROUND AND COMMENTS
-router.delete("/:id/delete", isLoggedIn, function (req, res) {
+router.delete("/:id/delete", checkCampgroundOwnership, function (req, res) {
     // Find the campground
     Campground.findById(req.params.id, function (err, campground) {
         if (err) {
@@ -94,7 +92,7 @@ router.delete("/:id/delete", isLoggedIn, function (req, res) {
                     }
                 });
             });
-    Campground.findByIdAndRemove(req.params.id, function (err) {
+            Campground.findByIdAndRemove(req.params.id, function (err) {
                 if (err) {
                     console.log(err);
                 } else {
@@ -104,9 +102,31 @@ router.delete("/:id/delete", isLoggedIn, function (req, res) {
             });
         }
     });
-}); 
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
+});
+//Checking the author of the campground
+function checkCampgroundOwnership(req, res, next) {
+    //is user logged in
+    if (req.isAuthenticated()) {
+        Campground.findById(req.params.id, (err, foundCampground) => {
+            if (err) {
+                res.redirect("/back");
+            } else {
+                //does user own the campground?
+                if (foundCampground.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        console.log("YOU NEED TO BE LOGGED IN TO DO THAT!!");
+        res.redirect("back");
+    }
+}
+//Checking if the user is logged in
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
         return next();
     }
     res.redirect("/login");
